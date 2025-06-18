f34a43b448baac10a24b3a6af341bea9939d5882
mayankdas2005 <mayankdas2005@gmail.com>
2025-06-18 22:56:17 +0530

    Added preliminary routes!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx"
+import Login from "./pages/LogIn.jsx"
+import Settings from './pages/Settings.jsx'
+import Signup from './pages/Signup.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/settings' element={<Settings/>} />
+      </Routes>
+    
+    
+    
+    
+    
+    </BrowserRouter>
   )
+    
+  
 }
 
 export default App
