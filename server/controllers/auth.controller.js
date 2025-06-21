0e9e3b5e8928f1e279301d19d2a9bcb9be83b058
mayankdas2005 <mayankdas2005@gmail.com>
2025-06-21 11:19:40 +0530

    ignore last one, this is the correct change

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const login = async(req, res) => {
         if(!validPassword) return res.status(401).json({message:"Invalid credentials"});
         const token = jwt.sign({id:correctUser._id},process.env.JWT_SECRET)
         const {password:hashedPass, ...rest} = correctUser._doc; 
-        res.cookie("acces_token", token, {httpOnly:true, expires: new Date(Date.now() + 3600000)}).status(200).json({message:"Login Succesfull",value: correctUser})
+        res.cookie("acces_token", token, {httpOnly:true, expires: new Date(Date.now() + 3600000)}).status(200).json({message:"Login Succesfull",value: rest})
          
 
 
